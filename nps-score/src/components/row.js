572181f1d7bf976a5b42f24980a604fe183d7793
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-22 23:56:49 -0800

    Create row.js

---
+++ nps-score/src/components/row.js
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from "react";
+import {
+  updatePromoters,
+  updatePassives,
+  updateDetractors,
+  toggleClear
+} from "../actions";
+import { connect } from "react-redux";
+import Card from "./card";
+
+function Row(props) {
+
+  const [reset, setReset] = useState(false)
+
+  let {
+    row,
+    header,
+    body,
+    totalPromoters,
+    totalPassives,
+    totalDetractors,
+    calculate,
+    clear
+  } = props;
+
+  useEffect(() => {
+    if(reset === true){
+      props.toggleClear(clear)
+      props.updatePromoters(0)
+      props.updatePassives(0)
+      props.updateDetractors(0)
+      setReset(false)
+    }
+  }, [reset])
+
+
+  // ***CONDITIONAL ELEMENTS***
+
+  // element to count responses and sum the totals
+  const countAndGroup = (
+    <>
+      <Card
+        start={10}
+        end={9}
+        type="promoters"
+        calculate={calculate}
+        action={props.updatePromoters}
+        totalPromoters={totalPromoters}
+        clear={reset}
+      />
+
+      <Card
+        start={8}
+        end={7}
+        type="passives"
+        calculate={calculate}
+        action={props.updatePassives}
+        totalPassives={totalPassives}
+        clear={clear}
+      />
+
+      <Card
+        start={6}
+        end={0}
+        type="detractors"
+        calculate={calculate}
+        action={props.updateDetractors}
+        totalDetractors={totalDetractors}
+        clear={clear}
+      />
+    </>
+  );
+
+  // element to calculate percentages
+  const percentages = (
+    <>
+      <Card
+        type="promoters"
+        calculate={calculate}
+        action={props.updatePromoters}
+        totalPromoters={totalPromoters}
+        totalPassives={totalPassives}
+        totalDetractors={totalDetractors}
+      />
+
+      <div className="column column-3">
+        <div className="operator operator-minus">&minus;</div>
+      </div>
+
+      <Card
+        type="detractors"
+        calculate={calculate}
+        action={props.updateDetractors}
+        totalPromoters={totalPromoters}
+        totalPassives={totalPassives}
+        totalDetractors={totalDetractors}
+      />
+
+      <div className="column column-5">
+        <div className="operator operator-equal">=</div>
+      </div>
+
+      <Card
+        type="nps"
+        calculate="nps"
+        totalPromoters={totalPromoters}
+        totalPassives={totalPassives}
+        totalDetractors={totalDetractors}
+      />
+    </>
+  );
+
+  // main row component
+  return (
+    <div className={`row row-${row}`}>
+      <div className="column column-1">
+        <div className="instructions">
+          <h3>{header}</h3>
+          <p>
+            {body}
+            {startOver(calculate)}
+          </p>
+        </div>
+      </div>
+      {renderComponent(calculate)}
+    </div>
+  );
+
+  // ***HELPER FUNCTIONS***
+
+  // render component depending on what you want to calculate
+  function renderComponent(calc) {
+    if (calc === "totals" || calc === "inputs") {
+      return countAndGroup;
+    } else if (calc === "percentages") {
+      return percentages;
+    }
+  }
+
+  function toggle(e){
+    setReset(true)
+  }
+
+  function startOver(calc) {
+    if (calc === "percentages") {
+      return (
+          <div>
+          <a className="clear-link" href="#" onClick={toggle}>
+            Start over
+          </a>
+          </div>
+        
+      );
+    }
+  }
+}
+
+// redux connection
+const mapStateToProps = ({ totals }) => ({
+  totalPromoters: totals.totalPromoters,
+  totalPassives: totals.totalPassives,
+  totalDetractors: totals.totalDetractors,
+  clear: totals.clear
+});
+
+export default connect(mapStateToProps, {
+  updatePromoters,
+  updatePassives,
+  updateDetractors,
+  toggleClear
+})(Row);
