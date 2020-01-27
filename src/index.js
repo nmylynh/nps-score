2e7ad1b6725f91ef219ef793815ec1d428268110
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-27 02:25:02 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
+import { createStore } from "redux";
 
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
