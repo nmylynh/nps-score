faaa66e55c50c0d2e117ff426f5a10251eeb358a
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-26 21:46:30 -0800

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import totalsReducer from './totalsReducer';
 import authReducer from './authReducer';
+import npsReducer from './npsReducer'
 
 export default combineReducers({
+    nps: npsReducer,
     totals: totalsReducer,
     auth: authReducer
 });
