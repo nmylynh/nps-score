45a043414b83f2929027e409be188650849919c7
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-27 01:14:12 -0800

    Update totalsReducer.js

--- src/reducers/totalsReducer.js
+++ src/reducers/totalsReducer.js
@@ -1,44 +1,44 @@
 import {
-    UPDATE_PROMOTERS,
-    UPDATE_PASSIVES,
-    UPDATE_DETRACTORS,
-    CLEAR_FORM 
-} from '../actions'
+  UPDATE_PROMOTERS,
+  UPDATE_PASSIVES,
+  UPDATE_DETRACTORS,
+  CLEAR_FORM
+} from "../actions";
 
 const initialState = {
-    totalPromoters: 0,
-    totalPassives: 0,
-    totalDetractors: 0,
-    clear: false
-}
+  totalPromoters: 0,
+  totalPassives: 0,
+  totalDetractors: 0,
+  clear: false
+};
 
 const totalsReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case UPDATE_PROMOTERS:
-            return {
-                ...state,
-                totalPromoters: action.payload
-            };
+  switch (action.type) {
+    case UPDATE_PROMOTERS:
+      return {
+        ...state,
+        totalPromoters: action.payload
+      };
 
-        case UPDATE_PASSIVES:
-            return {
-                ...state,
-                totalPassives: action.payload
-            };
-        
-        case UPDATE_DETRACTORS:
-            return {
-                ...state,
-                totalDetractors: action.payload
-            }
-        case CLEAR_FORM:
-            return {
-                ...state,
-                clear: action.payload
-            }
-        default:
-            return state
-    }
+    case UPDATE_PASSIVES:
+      return {
+        ...state,
+        totalPassives: action.payload
+      };
+
+    case UPDATE_DETRACTORS:
+      return {
+        ...state,
+        totalDetractors: action.payload
+      };
+    case CLEAR_FORM:
+      return {
+        ...state,
+        clear: action.payload
+      };
+    default:
+      return state;
+  }
 };
 
 export default totalsReducer;
