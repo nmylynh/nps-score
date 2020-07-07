91e62a2a12ad9a775f3f0b175f75f2563059c95f
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-07-06 23:52:49 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-
-import { createStore } from "redux";
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+
 
 ReactDOM.render(
   <Provider store={store}>
